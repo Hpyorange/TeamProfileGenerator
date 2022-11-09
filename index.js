c891c72c261f9f26fc8b5af59ea055a927f2d2b5
Hpyorange <jaspercheng2022@gmail.com>
2022-11-09 00:27:20 -0500

    add test & src folder

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const generateHtml= require('./src/generateHtml');
+
 const fs = require("fs")
 const inquirer = require("inquirer");
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
